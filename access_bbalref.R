3b33a905e6b134b12dd5973f98ebd98f516d7d89
iveksl2 <igor.veksler@avantcredit.com>
2015-03-08 01:30:39 -0600

    get real plus-minus df

--- access_bbalref.R
+++ access_bbalref.R
@@ -1,4 +1,5 @@
 ## first attempt at accessing basketball-reference.com (bible for nba statistics)
+require(plyr)
 library(XML)
 
 url <- 'http://www.basketball-reference.com/'
@@ -6,3 +7,13 @@ data <- XML::readHTMLTable(url, stringsAsFactors = FALSE)
 ## team simple rating system that takes into account point differential & strength of schedule 
 dat <- data[['srs_ratings']]
 
+## Access full RPM 
+fetch_partial_rpm <- function(page_num) {
+  url <- paste0("http://espn.go.com/nba/statistics/rpm/_/page/", page_num, "/sort/RPM")
+  data <- XML::readHTMLTable(url, stringsAsFactors = FALSE)
+  data[[1]]
+}
+
+full_rpm <- plyr::ldply(seq(13), fetch_partial_rpm, .progress = 'text')
+numeric_vars <- c( "GP", "MPG", "ORPM", "DRPM", "RPM", "WAR")
+full_rpm[, numeric_vars] <- sapply(full_rpm[, numeric_vars], as.numeric)
