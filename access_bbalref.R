02b832e81cd16ebe3e5e20193257782e9f810454
Igor Veksler <iveksl2@gmail.com>
2015-03-08 01:37:00 -0600

    Update access_bbalref.R

--- access_bbalref.R
+++ access_bbalref.R
@@ -4,8 +4,7 @@ library(XML)
 
 url <- 'http://www.basketball-reference.com/'
 data <- XML::readHTMLTable(url, stringsAsFactors = FALSE)
-## team simple rating system that takes into account point differential & strength of schedule 
-dat <- data[['srs_ratings']]
+dat <- data[['srs_ratings']] # team simple rating system that takes into account point differential & strength of schedule 
 
 ## Access full RPM 
 fetch_partial_rpm <- function(page_num) {
@@ -16,4 +15,7 @@ fetch_partial_rpm <- function(page_num) {
 
 full_rpm <- plyr::ldply(seq(13), fetch_partial_rpm, .progress = 'text')
 numeric_vars <- c( "GP", "MPG", "ORPM", "DRPM", "RPM", "WAR")
+## cast correct columns as numeric
 full_rpm[, numeric_vars] <- sapply(full_rpm[, numeric_vars], as.numeric)
+
+arrange(full_rpm , -RPM)
