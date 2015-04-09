89a016788d64b149ed5a9e17c16b89474bd19231
iveksl2 <igor.veksler@avantcredit.com>
2015-04-09 00:39:50 -0500

    add exp_pts func

--- bball_data/R/get_bball_data.R
+++ bball_data/R/get_bball_data.R
@@ -2,6 +2,8 @@
 ### GET DATA
 require(plyr)
 library(XML)
+library(magrittr)
+library(dplyr)
 
 #' Retrieves current snapshot of team SRS data (taken from bball-ref)
 #' team simple rating system that takes into account point differential & strength of schedule 
@@ -28,6 +30,34 @@ get_full_rpm <-  function() {
   ## convert to numeric  -> decided to do here instead of helper for performance optimization 
   numeric_vars <- c( "GP", "MPG", "ORPM", "DRPM", "RPM", "WAR")
   full_rpm[, numeric_vars] <- sapply(full_rpm[, numeric_vars], as.numeric)
+  full_rpm
 }
 
+#' Retrieves Team Level offensive and Defensive Efficienty 
+#' @export
+get_eff <- function() {
+  url <- 'http://espn.go.com/nba/hollinger/teamstats/_/sort/defensiveEff/order/false'
+  data <- XML::readHTMLTable(url, stringsAsFactors = FALSE, header = TRUE, as.data.frame = TRUE)
+  dat <- data[[1]]
+  names(dat) <- dat[1, ]  
+  names(dat) <- sub(" ", ".", names(dat))
+  dat <- dat[-1, ]
+}
 
+#' Calculates total Expeted Points Scored of 2 Arbitrary Teams 
+#' @export
+exp_pts <- function(teamA, teamB) {
+  dat <- get_eff()
+  dat2 <- dat[dat$TEAM %in% c(teamA, teamB), ] ## TODO can make it more versatile with grep  
+  stopifnot(nrow(dat2) == 2)
+  a_pace <- as.numeric(as.character(dat2[dat2$TEAM == teamA, 'PACE'])) 
+  a_off_eff<- as.numeric(as.character(dat2[dat2$TEAM == teamA, 'OFF.EFF'])) 
+  a_def_eff<- as.numeric(as.character(dat2[dat2$TEAM == teamA, 'DEF.EFF'])) 
+  b_pace <- as.numeric(as.character(dat2[dat2$TEAM == teamB, 'PACE'])) 
+  b_off_eff <- as.numeric(as.character(dat2[dat2$TEAM == teamB, 'OFF.EFF'])) 
+  b_def_eff <- as.numeric(as.character(dat2[dat2$TEAM == teamB, 'DEF.EFF'])) 
+
+  a_exp_pts <- a_pace / 100 * a_off_eff * ( b_def_eff / 100)
+  b_exp_pts <- b_pace / 100 * b_off_eff * ( a_def_eff / 100)
+  a_exp_pts + b_exp_pts
+}
