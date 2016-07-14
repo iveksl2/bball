df5ba3f03b298906addbf1000ce5214fd755d116
iveksl2 <iveksl2@gmail.com>
2016-07-14 06:28:53 -0500

    is_regular_season implementation

--- R-Package/R/is_regular_season.R
+++ R-Package/R/is_regular_season.R
@@ -1,21 +1,18 @@
 REG_SEASON_START_MONTH <- 10
 REG_SEASON_START_DAY   <- 25 
-REG_SEASON_END_MONTH   <- 3 
+REG_SEASON_END_MONTH   <- 4 
 REG_SEASON_END_DAY     <- 16 
 
 #' Whether the nba game date falls within regular season. 
 #' Games outside of this fall within the playoffs. Note there are lockout seasons not currently handled
 #'
 #' @export
-is_regular_season <- function(date) {
-  stopifnot(is.Date(date))
-  gm_month <- lubridate::month(date)
-  gm_day   <- lubridate::month(date)
-  if (gm_month >= REG_SEASON_START_MONTH & gm_month <= REG_SEASON_END_MONTH) {
-    if(gm_month == REG_SEASON_END_DAY) { # Dont check start month as any game ~ October is regular season. Specific start days vary 
-      if (gm_day > REG_SEASON_END_DAY) FALSE
-    }
-    else TRUE
-  }    
-  FALSE
+is_regular_season <- function(gm_date) {
+  stopifnot(is.Date(gm_date))
+  gm_month <- lubridate::month(gm_date)
+  gm_day   <- lubridate::day(gm_date)
+  if (gm_month %in% c(11, 12, 1, 2, 3)) TRUE 
+  else if (identical(gm_month, REG_SEASON_START_MONTH) & gm_day >= REG_SEASON_START_DAY) TRUE 
+  else if (identical(gm_month, REG_SEASON_END_MONTH)   & gm_day <= REG_SEASON_END_DAY)   TRUE
+  else FALSE
 }
--- R-Package/tests/testthat/test-is_regular_season.R
+++ R-Package/tests/testthat/test-is_regular_season.R
@@ -9,14 +9,13 @@ test_that('errors on non-date input', {
 test_that('successfully classifies regular season games', {
   expect_true(is_regular_season(as.Date('2015-01-01')))
   expect_true(is_regular_season(as.Date('2015-10-25')))
-  expect_true(is_regular_season(as.Date('2015-10-21'))) # season starts early
-  expect_true(is_regular_season(as.Date('2015-03-10'))) 
+  expect_true(is_regular_season(as.Date('2015-04-10'))) 
   expect_true(is_regular_season(as.Date('2015-12-12'))) 
 })
 
 
 test_that('doesent assign playoff or summer league games to regular season', {
-  expect_false(is_regular_season(as.Date('2015-03-20')))
-  expect_false(is_regular_season(as.Date('2015-05-30')))
+  expect_false(is_regular_season(as.Date('2015-04-20')))
+  expect_false(is_regular_season(as.Date('2015-07-30')))
   expect_false(is_regular_season(as.Date('2014-09-15')))
 })
