068d4dcb557e824ca913065421563f67e4f7b180
iveksl2 <iveksl2@gmail.com>
2016-08-15 15:13:01 -0500

    extracting team total elements

--- scrappers/full_team_box_scores.py
+++ scrappers/full_team_box_scores.py
@@ -1,4 +1,5 @@
 """ 
+http://www.basketball-reference.com/boxscores/201603220BRK.html
 """
 from bs4 import BeautifulSoup
 from datetime import date
@@ -8,17 +9,37 @@ import pandas as pd
 import pdb
 import time
 
-def make_soup(url):
+def soup_from_url(url):
+""" url -> SoupObj ; Instantiate Beautiful Soup Object from a url.
+"""    
 	response = requests.get(url)
 	html     = response.content
 	soup     = BeautifulSoup(html, 'lxml')
 	return soup
 
+def extract_simple_box_score_stats(team_tot_tags):
+    """bs4.element.Tag -> Numeric List; extracts Team Basic Box Score Statistics""" 
+    return [float(elem.get_text()) for elem in team_tot_tags.find_all('td')[1:-1]]
+
+def extract_adv_box_score_stats(team_tot_tags):
+    """bs4.element.Tag -> Numeric List; extracts Team Advanced Box Score Statistics"""
+    return [float(elem.get_text()) for elem in team_tot_tags.find_all('td')[1:]]
 
 def main():
-# key line
-soup.find_all('tfoot')
+    url = 'http://www.basketball-reference.com/boxscores/201603220BRK.html'
+    soup = soup_from_url(url)
+    soup.find_all('tfoot')
 
+    team_totals = soup.find_all('tfoot') 
+    [float(elem.get_text()) for elem in team_totals[1:-1]] 
+
+    # I don't like this style. If there is a way to make it more consise? 
+    away_basic_team_stats = team_totals[0]
+    away_adv_team_stats   = team_totals[1]
+    home_basic_team_stats = team_totals[2]
+    home_adv_team_stats   = team_totals[3]
+    
+ 
 
 if __name__ == "__main__":
 	main()
