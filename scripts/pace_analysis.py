0755afaff4c8596199418aab8b14cd281ea2898a
Igor Veksler <igor.veksler@igor.veksler MacBook>
2016-12-12 22:03:56 -0600

    return sorted df when long to wide

--- scripts/pace_analysis.py
+++ scripts/pace_analysis.py
@@ -16,20 +16,18 @@ def re_arrange_first_col(df, col_name):
 def wide_to_long(df, team):
 	''' Coverts wide dataframen(hometeam & awayteam) by stacking on a team basis'''
         # todo: DRY
-	home     = df[df.home_team == team]
-	home_sub = home.iloc[:, home.columns.str.contains('date|home|pace|id')]
-	home_sub.columns = home_sub.columns.str.replace('home_', '')
+	home_df       = df[df.home_team == team]
+	home_df_cols  = home_df.iloc[:, home_df.columns.str.contains('date|home|pace|id')]
+	home_df_cols.columns = home_df_cols.columns.str.replace('home_', '')
 
-	away     = df[df.away_team == team]
-	away_sub = away.iloc[:, away.columns.str.contains('date|away|pace|id')]
-	away_sub.columns = away_sub.columns.str.replace('away_', '')
+	away_df       = df[df.away_team == team]
+	away_df_cols  = away_df.iloc[:, away_df.columns.str.contains('date|away|pace|id')]
+	away_df_cols.columns = away_df_cols.columns.str.replace('away_', '')
 
-	long_df  = pd.concat([home_sub, away_sub])
-	return long_df
-
-def calculate_days_rest(df):
-    df.date - df.date.shift(1)
-    return days_rest_delta.days
+	long_df  = pd.concat([home_df_cols, away_df_cols]) 
+        long_df.drop(['team_spread', 'team_moneyline', 'game_date'], axis = 1, inplace = True)
+        stacked_df = long_df.sort_values(by = ['date']) 
+	return stacked_df 
 
 # --------------------   workflow   --------------------------
 pd.set_option('display.width', 150) # todo: put this in a config file to persist 
@@ -39,6 +37,7 @@ df['game_id']     = range(df.shape[0])
 pd.options.display.max_columns = 50
 df['date']        = pd.to_datetime(df['date'])
 df['away_team']   = df['away_team'].str.lower() #todo, do this upsream
+df['home_team']   = df['home_team'].str.lower() #todo, do this upsream
 df['total_score'] = df['home_finalscore'] + df['away_finalscore']
 df                = re_arrange_first_col(df, 'game_id')
 
@@ -50,31 +49,35 @@ rolling_team_avgs = {}
 for team in set(df.home_team.values): 
     rolling_team_avgs[team] = wide_to_long(df, team)
     rolling_team_avgs[team]['game_id']  = rolling_team_avgs[team]['game_id'].astype(str) # hack to prevent rolling game_id 
-    rolling_team_avgs[team]['date']  = rolling_team_avgs[team]['date'].astype(str) # hack to prevent rolling game_id 
-    rolling_team_avgs[team].drop(['team_spread', 'team_moneyline'], axis = 1, inplace = True) 
+    rolling_team_avgs[team]['date']  = rolling_team_avgs[team]['date'].astype(str) 
     rolling_team_avgs[team] = rolling_team_avgs[team].ewm(alpha = .15).mean()
     rolling_team_avgs[team]['game_id']   = rolling_team_avgs[team]['game_id'].astype(int) # hack to prevent rolling game_id 
-    rolling_team_avgs[team]['date']      = pd.to_datetime(rolling_team_avgs[team]['date'])   # hack 
+    rolling_team_avgs[team]['date']      = pd.to_datetime(rolling_team_avgs[team]['date'])   
     rolling_team_avgs[team]['days_rest'] = rolling_team_avgs[team]['date'] - rolling_team_avgs[team]['date'].shift(1)
-    rolling_team_avgs[team]['prev_game_id'] = rolling_team_avgs[team]['game_id'].shift(-1) # RHS Table
     rolling_team_avgs[team].dropna(inplace = True)
-    rolling_team_avgs[team]['days_rest']    = rolling_team_avgs[team]['days_rest'].apply(lambda x: x.days)
-    rolling_team_avgs[team]['prev_game_id'] = rolling_team_avgs[team]['prev_game_id'].astype(int)
+    rolling_team_avgs[team]['days_rest'] = rolling_team_avgs[team]['days_rest'].apply(lambda x: x.days)
+    rolling_team_avgs[team]['shifted_back_game_id'] = rolling_team_avgs[team]['game_id'].shift(-1) # RHS Table
+    rolling_team_avgs[team]['days_rest'] = rolling_team_avgs[team]['days_rest'].shift(-1) 
+    rolling_team_avgs[team].dropna(inplace = True)
+    rolling_team_avgs[team]['days_rest'] = rolling_team_avgs[team]['days_rest'].astype(int)
+    rolling_team_avgs[team]['shifted_back_game_id'] = rolling_team_avgs[team]['shifted_back_game_id'].astype(int)
     rolling_team_avgs[team].drop(['date'], axis = 1, inplace = True) 
 
 
-vegas_data = df.loc[:, ['game_id', 'date', 'home_team', 'away_team', 'total_score', 'over_under']]
+vegas_data = df[['game_id', 'date', 'home_team', 'away_team', 'total_score', 'over_under']]
 vegas_data.index = vegas_data['date']
 
 rolling_avg_concat = pd.concat(rolling_team_avgs.values(), ignore_index = True) 
-vegas_with_rolling_figs = vegas_data.copy() 
-vegas_with_rolling_figs  =  pd.merge(vegas_with_rolling_figs, rolling_avg_concat , left_on = ['home_team', 'game_id'],   right_on = ['team', 'prev_game_id'], how = 'left')
-vegas_with_rolling_figs  =  pd.merge(vegas_with_rolling_figs, rolling_avg_concat , left_on = ['away_team', 'game_id_x'],   right_on = ['team', 'prev_game_id'], how = 'left')
+
+vegas_dat_merged = vegas_data.copy() 
+vegas_dat_merged  =  pd.merge(vegas_dat_merged, rolling_avg_concat , left_on = ['home_team', 'game_id'],   right_on = ['team', 'shifted_back_game_id'], how = 'left')
+vegas_dat_merged  =  pd.merge(vegas_dat_merged, rolling_avg_concat , left_on = ['away_team', 'game_id_x'],   right_on = ['team', 'shifted_back_game_id'], how = 'left')
 
 
-# ------ modeling workflow to transfer -------------
-o_u_dat = vegas_with_rolling_figs.dropna()
+# ------ mod  -------------
+o_u_dat = vegas_dat_merged.dropna()
 o_u_dat = o_u_dat[o_u_dat.over_under > 100] 
+
 o_u_dat['partition'] = create_train_test_col(o_u_dat)
 
 train_dat    = o_u_dat[o_u_dat.partition == 'train']
@@ -83,30 +86,16 @@ train_target = train_dat.total_score
 test_dat    = o_u_dat[o_u_dat.partition == 'test']
 test_target = test_dat.total_score 
 
-rf = RandomForestClassifier(n_estimators = 500)
-rf2 = RandomForestClassifier(n_estimators = 500)
+rf = RandomForestClassifier(n_estimators = 1000)
 
-features            = list(set(vegas_with_rolling_figs.columns) - set(['game_id_x', 'game_id_y', 'home_team', 'away_team', 'team_x', 'team_y', 'date', 'game_date', 'game_date_x', 'game_date_y', 'partition', 'date_x', 'date_y', 'total_score'])) 
-features2  = list(set(vegas_with_rolling_figs.columns) - set(['game_id_x', 'game_id_y', 'home_team', 'away_team', 'team_x', 'team_y', 'date', 'game_date', 'game_date_x', 'game_date_y', 'partition', 'date_x', 'date_y', 'total_score', 'over_under'])) 
+features            = list(set(vegas_dat_merged.columns) - set(['game_id_x', 'game_id_y', 'home_team', 'away_team', 'team_x', 'team_y', 'date', 'game_date', 'game_date_x', 'game_date_y', 'partition', 'date_x', 'date_y', 'total_score'])) 
 
 rf.fit(train_dat[features], train_target)
-rf2.fit(train_dat[features2], train_target)
 
 test_dat['model_o_u'] = rf.predict(test_dat[features])
-test_dat['model_o_u2'] = rf2.predict(test_dat[features2])
 
-val_df  = test_dat[['total_score', 'over_under', 'model_o_u', 'model_o_u2']]
+val_df  = test_dat[['total_score', 'over_under', 'model_o_u']]
 val_df['dif'] = val_df['model_o_u'] - val_df['over_under']
-val_df['dif2'] = val_df['model_o_u2'] - val_df['over_under']
-
-diff_threshold = 0 
-
-for diff_threshold in range(10):
-    bet_over_df  = val_df[val_df.dif > diff_threshold]
-    bet_under_df = val_df[val_df.dif < -diff_threshold]
-    print('diff threshold is ' + str(diff_threshold))
-    print('over win % ' + str(np.mean(bet_over_df.total_score > bet_over_df.over_under)))
-    print('under win & ' + str(np.mean(bet_under_df.total_score < bet_under_df.over_under)))
 
 
 diff_threshold = 0 
@@ -114,6 +103,6 @@ val_df['dif'] = val_df['model_o_u'] - val_df['over_under']
 bet_over_df  = val_df[val_df.dif > diff_threshold]
 bet_under_df = val_df[val_df.dif < -diff_threshold]
 
-np.mean(bet_over_df.total_score > bet_over_df.over_under)
-np.mean(bet_under_df.total_score < bet_under_df.over_under)
+print(np.mean(bet_over_df.total_score > bet_over_df.over_under))
+print(np.mean(bet_under_df.total_score < bet_under_df.over_under))
 
