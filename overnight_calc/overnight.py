f7fe9c9e7f8af0c350c6cfd50e8f6893344f080a
iveksl2 <iveksl2@gmail.com>
2017-02-20 00:15:31 -0600

    access bpm via selenium xpath

--- overnight_calc/overnight.py
+++ overnight_calc/overnight.py
@@ -1,5 +1,4 @@
 # http://www.basketball-reference.com/teams/CLE/2017.html
-
 from bs4 import BeautifulSoup
 from datetime import date
 import requests
@@ -7,7 +6,7 @@ import re
 import pandas as pd
 import pdb
 import time
-#from selenium import webdriver
+from selenium import webdriver
 from bs4 import BeautifulSoup
 
 # constants
@@ -21,30 +20,30 @@ team = teams[6]
 
 url = 'http://www.basketball-reference.com/teams/' + team + '/2017.html'
 
-def soup_from_url(url):
+def get_soup(url):
     """ url -> SoupObj ; Instantiate Beautiful Soup Object from a url """    
     response = requests.get(url)
     html     = response.content
     soup     = BeautifulSoup(html, 'lxml')
     return soup
 
+# beautiful soup approach (doesent work)
+soup = get_soup(url)
 
-soup = soup_from_url(url)
-pok = soup.find('div', attrs = {'id' : 'all_per_game'})
-pok2 = pok.findChild()
-pok3 = pok2.find_next_sibling()
+driver = webdriver.Chrome(executable_path='/Users/igor.veksler/Desktop/chromedriver')
 
+driver.get(url)
+# mpg
+advanced_rows = driver.find_elements_by_xpath('//*[@id="advanced"]/tbody/tr')
+advanced_rows[0].find_elements_by_xpath('child::*')[1].text
+# bpm
+per_game_rows = driver.find_elements_by_xpath('//*[@id="per_game"]/tbody/tr')
+per_game_rows[0].find_elements_by_xpath('child::*')[1].text
 
-#yo  = soup.find('table', attrs = {'id': 'per_game'})
-#yo2  = pok.find('table', attrs = {'id': 'per_game'})
-#
-#pok2 = pok.findChildren()
-#pok2 = pok.children
+##selenium approach (doesent work)
+#chrome_driver = webdriver.Chrome("/Users/igor.veksler/Desktop/chromedriver") 
+#chrome_driver.get('http://www.basketball-reference.com/teams/OKC/2017.html')
+#selenium_soup = BeautifulSoup(chrome_driver.page_source, "html.parser")
+#pace = selenium_soup.find("td", {"data-stat":"pace"})
 
-
-pok = soup.find('div', attrs = {'id' : 'table'})
-pok = soup.find('div', attrs = {'class' : 'table_outer_container'})
-
-pok = soup.find('table', attrs = {'id' : "per_game"})
-pok = soup.find('table')
 
