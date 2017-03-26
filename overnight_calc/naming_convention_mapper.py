822fbf4696d949838cf286f680724a04997da27e
iveksl2 <iveksl2@gmail.com>
2017-03-26 16:15:30 -0500

    Add naming convention mapper dict

---
+++ overnight_calc/naming_convention_mapper.py
@@ -0,0 +1,21 @@
+#TODO: DRY in 1 file. Duplicate code
+BBALL_REF_TEAM_MAP = {
+    'ATLANTA HAWKS'         : 'ATL', 'BOSTON CELTICS'        : 'BOS',
+    'BROOKLYN NETS'         : 'BRK', 'CHARLOTTE BOBCATS'     : 'CHA',
+    'CHARLOTTE HORNETS'     : 'CHO', 'CHICAGO BULLS'         : 'CHI',
+    'CLEVELAND CAVALIERS'   : 'CLE', 'DALLAS MAVERICKS'      : 'DAL',
+    'DENVER NUGGETS'        : 'DEN', 'DETROIT PISTONS'       : 'DET',
+    'GOLDEN STATE WARRIORS' : 'GSW', 'HOUSTON ROCKETS'       : 'HOU',
+    'INDIANA PACERS'        : 'IND', 'LOS ANGELES CLIPPERS'  : 'LAC',
+    'LOS ANGELES LAKERS'    : 'LAL', 'MEMPHIS GRIZZLIES'     : 'MEM',
+    'MIAMI HEAT'            : 'MIA', 'MILWAUKEE BUCKS'       : 'MIL',
+    'MINNESOTA TIMBERWOLVES': 'MIN', 'NEW JERSEY NETS'       : 'NJN',
+    'NEW ORLEANS PELICANS'  : 'NOP', 
+    'NEW ORLEANS HORNETS'   : 'NOH',
+    'NEW YORK KNICKS'       : 'NYK', 'OKLAHOMA CITY THUNDER' : 'OKC',
+    'ORLANDO MAGIC'         : 'ORL', 'PHILADELPHIA 76ERS'    : 'PHI',
+    'PHOENIX SUNS'          : 'PHO', 'PORTLAND TRAIL BLAZERS': 'POR',
+    'SACRAMENTO KINGS'      : 'SAC', 'SAN ANTONIO SPURS'     : 'SAS',
+    'SEATTLE SUPERSONICS'   : 'SEA', 'TORONTO RAPTORS'       : 'TOR',
+    'Utah Jazz'             : 'UTA', 'WASHINGTON WIZARDS'    : 'WAS'
+}
